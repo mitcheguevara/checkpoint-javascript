d226712376c0152142ec47f2c7ccd4e7690c42db
YOUR FULL NAME <THE_EMAIL_YOU_USE_FOR_GITHUB@EMAIL.COM>
2017-08-18 10:18:34 -0400

    checkpoint javacript

--- fundamentals.js
+++ fundamentals.js
@@ -1,31 +1,49 @@
-// NOTE: Make sure to use the `var` keyword for ALL variable declarations.
+// NOTE: vMake sure to use the `var` keyword for ALL variable declarations.
+var color = ('red', 'blue', 'black')
+
+
 
 // #1: Create an array of strings called `colors` that contains three colors.
 // Type your solution immediately below this line:
-
+var color = ('red', 'blue', 'black')
 
 
 // #2: Access the last item in the array and assign to a variable called `last`.
 // Type your solution immediately below this line:
+var myArr = [red, blue, black]
+console.log(myArr[2])
+
+
 
 
 
 // #3: Create an empty array called `favoriteColors`.
 // Type your solution immediately below this line:
-
+var myArr ='favoriteColors'
 
 
 // #4: Create a `for` loop that adds each string in `colors` to `favoriteColors`.
 // Type your solution immediately below this line:
+var favoriteColors = ["red", "blue", "blck"]
 
+for (var i =0; i <favoriteColors.length; i++) {
+  console.log(favoriteColors[i])
+
+}
 
 
 // #5: Create an object literal called `instructor` that contains three key-value pairs.
 // Type your solution immediately below this line:
-
-
-
+var instructor = {
+  name: 'damien',
+  birth: 'Washington DC',
+  age: 22
+}
 // #6: Add a `facial-hair` (spelled exactly) property to `instructor` by accessing
 // it (do not change the original object you typed above) and assigning it
 // a boolean value.
 // Type your solution immediately below this line:
+facial-hair.instructor
+
+instructor == damien
+true
