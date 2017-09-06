441b7e33a66e5561a384ea6ef4d6baa701c7db6e
YOUR FULL NAME <THE_EMAIL_YOU_USE_FOR_GITHUB@EMAIL.COM>
2017-09-06 13:37:19 -0400

    checkpoint3

--- fundamentals.js
+++ fundamentals.js
@@ -1,17 +1,28 @@
-// NOTE: vMake sure to use the `var` keyword for ALL variable declarations.
-var color = ('red', 'blue', 'black')
+// NOTE: vMake sure to use the `var` keyword for ALL variable declarations
 
 
 
 // #1: Create an array of strings called `colors` that contains three colors.
 // Type your solution immediately below this line:
-var color = ('red', 'blue', 'black')
+var colors = ['red', 'green', 'blue']
 
 
 // #2: Access the last item in the array and assign to a variable called `last`.
 // Type your solution immediately below this line:
-var myArr = [red, blue, black]
-console.log(myArr[2])
+
+var last = colors[2]
+
+
+
+//var myArr = ['red', 'blue', 'black']
+//myArr.search('black')
+
+//var myArr =$()
+
+//console.log(myArr[2])
+
+
+
 
 
 
@@ -19,18 +30,17 @@ console.log(myArr[2])
 
 // #3: Create an empty array called `favoriteColors`.
 // Type your solution immediately below this line:
-var myArr ='favoriteColors'
+var favoriteColors = ['']
 
 
 // #4: Create a `for` loop that adds each string in `colors` to `favoriteColors`.
 // Type your solution immediately below this line:
-var favoriteColors = ["red", "blue", "blck"]
+var favoriteColors = ['red', 'green', 'blue']
 
 for (var i =0; i <favoriteColors.length; i++) {
   console.log(favoriteColors[i])
 
 }
-
 
 // #5: Create an object literal called `instructor` that contains three key-value pairs.
 // Type your solution immediately below this line:
@@ -38,12 +48,17 @@ var instructor = {
   name: 'damien',
   birth: 'Washington DC',
   age: 22
-}
+};
+
+
+
+
+
+//instructor.("facial-hair") = "false"
 // #6: Add a `facial-hair` (spelled exactly) property to `instructor` by accessing
 // it (do not change the original object you typed above) and assigning it
 // a boolean value.
 // Type your solution immediately below this line:
-facial-hair.instructor
+instructor["facial-hair"] = true;
 
-instructor == damien
-true
+//true
--- hof.js
+++ hof.js
@@ -1,6 +1,5 @@
 // NOTE: Make sure to use the `var` keyword for ALL variable declarations
-
-var people = [
+var peoplesAges =[
   {name: "Jane", age: 32},
   {name: "Jessica", age: 34},
   {name: "Jasmine", age: 39}
@@ -10,11 +9,14 @@ var people = [
 // person in the `people` array. Assign the returned array to a variable
 // called `peoplesAges`.
 // Type your solution immediately below this line:
-
-
-
+var peopleNames = people.map(function(person){
+  return person.name
+})
 
 // #2: Use the `filter` array method to create a new, filtered array containing only
 // persons from the `people` array who are older than 35. Assign the returned array
 // to a variable called `peopleOlderThan35`.
 // Type your solution immediately below this line:
+var peopleOlderThan35 = people.filter(function(person){
+  return person.age > 35
+})
